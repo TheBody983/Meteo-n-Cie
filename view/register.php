1b5b11b94fb5cdb65291de25d90d2ce63ec407a1
balbanivanfouqu <maitredragon16@gmail.com>
2020-09-17 11:50:17 +1100

    Modifcation/ajout mail

--- view/register.php
+++ view/register.php
@@ -2,19 +2,24 @@
 <?php ob_start(); ?>
 <form method="post" action="main">
     <label for="name"> Nom </label> :
-    <input type="text" name="name" id="name"/>
+    <input type="text" name="name" id="idname"/>
     <br />
     <label for="surname"> Prenom </label> :
-    <input type="text" name="surname" id="surname"/>
+    <input type="text" name="surname" id="idsurname"/>
+    <br />
+    <label for="mail"> E-Mail </label> :
+    <input type="text" name="mail" id="idmail"/>
+    <br />
+    <br />
     <br />
     <label for="idlogin"> Votre identifiant </label> :
-    <input type="text" name="login" id="idlogin"/>
+    <input type="text" name="login" id="idlogin" maxlength="12" required/>
     <br />
     <label for="idpassword"> Votre mot de passe </label> :
-    <input type="password" name="password" id="idpassword" />
+    <input type="password" name="password" id="idpassword" maxlength="12" required/>
     <br />
     <input type="submit" value="Envoyer"/>
 </form>
-<a href="http://localhost/Meteo-n-Cie/index.php/main"> Annuler </a>
+<a href="/Meteo-n-Cie/index.php/main"> Annuler </a>
 <?php $content = ob_get_clean(); ?>
 <?php include 'layout.php'; ?>
