51bb486b3d5bdd0c163048a4850579e2a59d319c
TheBody983 <61261544+TheBody983@users.noreply.github.com>
2020-09-17 11:05:39 +1100

    model.php 1
    new_user
    is_user

--- model.php
+++ model.php
@@ -2,7 +2,7 @@
 //DATABASE CONNECTION
 function open_database_connection()
 {
-    $link = mysqli_connect('localhost', 'root', '', 'annonces');
+    $link = mysqli_connect('localhost', 'model', '1234', 'meteo_n_cie');
     return $link;
 }
 
@@ -16,7 +16,7 @@ function close_database_connection($link)
 function get_all_users()
 {
     $link = open_database_connection();
-    $resultall = mysqli_query($link,'SELECT login, userID FROM users WHERE userID != "Server"');
+    $resultall = mysqli_query($link,'SELECT login, userID FROM users');
     $users = array();
     while ($row = mysqli_fetch_assoc($resultall)) {
         $users[] = $row;
@@ -29,52 +29,108 @@ function get_all_users()
 
 function getUserID($login)
 {
+    //Connexion à la BDD
     $link = open_database_connection();
-    $query = 'SELECT userID FROM users WHERE login="'.$login.'"';
-    $result = mysqli_query($link, $query);
-    if($result){
-        $id = mysqli_fetch_assoc($result);
-        mysqli_free_result( $result);
-    }
-    else $id = false;
+
+    //Securise la chaîne login
+    $login = htmlspecialchars($login);
+    $login =  str_replace(array('\n','\r',PHP_EOL),' ',$login);
+
+    //Prepare la requête
+    $query = mysqli_prepare($link,'SELECT userID FROM users WHERE login=?');
+    mysqli_stmt_bind_param($query, 's', $login);
+
+    //Execute la requête et récupère les résultats
+    mysqli_stmt_execute($query);
+    mysqli_stmt_bind_result($query, $userID);
+
+    //Fermeture de la connexion à la BDD
     close_database_connection($link);
-    return $id['userID'];
+
+    //Retourne le résultat
+    return $userID;
 }
 
 function getUserLogin($id)
 {
+    //Connexion à la BDD
     $link = open_database_connection();
-    $query = 'SELECT login FROM users WHERE userID="'.$id.'"';
-    $result = mysqli_query($link, $query);
-    if($result){
-        $login = mysqli_fetch_assoc($result);
-        mysqli_free_result( $result);
-    }
-    else $login = false;
+
+    //Securise la valeur id
+    $id = intval($id);
+
+    //Prepare la requête
+    $query = mysqli_prepare($link,'SELECT login FROM users WHERE userID=?');
+    mysqli_stmt_bind_param($query, 'i', $id);
+
+    //Execute la requête et récupère les résultats
+    mysqli_stmt_execute($query);
+    mysqli_stmt_bind_result($query, $login);
+
+    //Fermeture de la connexion à la BDD
     close_database_connection($link);
-    return $login['login'];
+
+    //Retourne le résultat
+    return $login;
 }
 
 function is_user( $login, $password )
 {
-$isuser = False ;
-$link = open_database_connection();
-$query= 'SELECT login, userID FROM Users WHERE login="'.$login.'" and password="'.$password.'"';
-$result = mysqli_query($link, $query );
-if( mysqli_num_rows( $result) )
-    $isuser = True;
-mysqli_free_result( $result );
-close_database_connection($link);
-return $isuser;
+    $isuser = False ;
+
+    //Connexion à la BDD
+    $link = open_database_connection();
+
+    //Securise la chaîne login
+    $login = htmlspecialchars($login);
+    $login =  str_replace(array('\n','\r',PHP_EOL),' ',$login);
+
+    //Prepare la requête
+    $query = mysqli_prepare($link,'SELECT password FROM users WHERE login=?');
+    mysqli_stmt_bind_param($query, 's', $login);
+
+    //Execute la requête
+    if(mysqli_stmt_execute($query)) {
+        echo 'executed <br />';
+
+        //Récupère le résultat
+        $query = mysqli_stmt_get_result($query);
+        $hash = mysqli_fetch_array($query, MYSQLI_NUM);
+        if(password_verify($password, $hash[0])) { //Vérifie si le mot de passe entré correspond au mot de passe stocké
+            echo "verified";
+            $isuser = True;
+        }
+    }
+
+    close_database_connection($link);
+    return $isuser;
 }
 
-function new_user($login,$pwd,$surname,$name,$mail,$country,$city){
+function new_user($login,$pwd){
     $link = open_database_connection();
-    $query= 'SELECT MAX(userID) AS ID FROM users' ;
-    $res = mysqli_query($link, $query );
-    $id = mysqli_fetch_assoc($res)['ID']+1;
-    $query= 'INSERT INTO users VALUES ("'.$id.'", "'.$login.'", "'.$pwd.'", "'.$surname.'", "'.$name.'", "'.$mail.'", "'.$country.'", "'.$city.'")' ;
-    mysqli_query($link, $query );
+
+    //Securise la chaîne login
+    $login = htmlspecialchars($login);
+    $login =  str_replace(array('\n','\r',PHP_EOL),' ',$login);
+
+    //Securise la chaîne pwd
+    $pwd = htmlspecialchars($pwd);
+    $pwd =  str_replace(array('\n','\r',PHP_EOL),' ',$pwd);
+
+    //hash le pwd
+    $pwd= password_hash($pwd, PASSWORD_DEFAULT);
+
+    //echo $login;
+    //echo $pwd;
+
+    //Prepare la requête
+    $query = mysqli_prepare($link,'INSERT INTO users(login, password) VALUES (?, ?)');
+    //echo mysqli_error( $link );
+    mysqli_stmt_bind_param($query, 'ss', $login, $pwd);
+
+    //execute la requête
+    mysqli_stmt_execute($query);
+
     close_database_connection($link);
 }
 
