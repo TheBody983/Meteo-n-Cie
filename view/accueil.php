c49939d9005bd6ddcafe4a331604c41eaf82a1a6
TheBody983 <61261544+TheBody983@users.noreply.github.com>
2020-09-23 14:25:50 +1100

    Ajout second CSS pour mes petits yeux

--- view/accueil.php
+++ view/accueil.php
@@ -9,9 +9,9 @@
 	<link rel="stylesheet" href="../index.css"/>
 	<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 </head>-->
-<body>
-	<section id="menu">
-		<img id='titremenu' src="../graphs/menu.png">
+<div class="container">
+	<div id="menu" class="containerCol">
+        <img id='titremenu' src="../graphs/menu.png"/>
         <button id="connexion" onclick="window.location.href = 'login';">Se connecter/ S'inscrire</button>
 		<p><a id="rubrique" href="">Liste des stations</a></p>
 		<!-- Envoie à la page souhaitée si connecté, sinon renvoit à la page de login -->
@@ -19,46 +19,46 @@
 			<p><a id="rubrique" href="">Messagerie</a></p>
 			<p><a id="rubrique" href="">Projet</a></p>
 			<p><a id="rubrique" href="donnees">Données</a></p>
-	</section>
-	<div id="mapid"></div>
-	<script>
+	</div>
 
-		var mymap = L.map('mapid').setView([-21, -194.55], 8);
 
-		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-			id: 'mapbox/streets-v11',
-			tileSize: 512,
-			zoomOffset: -1
-		}).addTo(mymap);
-
-		var marker = L.marker([-20.916709, -192.734699]).addTo(mymap);
-		marker.bindPopup("<b>Wé, Lifou</b><br><a href=''>Cliquez pour plus d'infos</a>");
-
-		var marker = L.marker([-22.297812, -193.561785]).addTo(mymap);
-		marker.bindPopup("<b>Baie des Citrons, Nouméa</b><br><a href=''>Cliquez pour plus d'infos</a>");
-
-		var marker = L.marker([-21.607084, -194.545169]).addTo(mymap);
-		marker.bindPopup("<b>Baie des Tortues, Bourail</b><br><a href=''>Cliquez pour plus d'infos</a>");
-
-		var popup = L.popup()
-		function onMapClick(e) {
-			popup
-				.setLatLng(e.latlng)
-				.setContent("Vous avez cliqué à " + e.latlng.toString())
-				.openOn(mymap);
-		}
-
-		mymap.on('click', onMapClick );
-		
-	</script>
+	<div id="mapid" ></div>
+</div>
 	<footer>
-		<div id="avertissement">AVERTISSEMENT:</div>
-		<div id="textavertissement"><marquee behavior="scroll">⚠ STATION WE, LIFOU HS </marquee></div>
-	</footer>
-</body>
+    </footer>
+    <script>
+
+        var mymap = L.map('mapid').setView([-21, -194.55], 8);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1
+        }).addTo(mymap);
+
+        var marker = L.marker([-20.916709, -192.734699]).addTo(mymap);
+        marker.bindPopup("<b>Wé, Lifou</b><br><a href=''>Cliquez pour plus d'infos</a>");
+
+        var marker = L.marker([-22.297812, -193.561785]).addTo(mymap);
+        marker.bindPopup("<b>Baie des Citrons, Nouméa</b><br><a href=''>Cliquez pour plus d'infos</a>");
+
+        var marker = L.marker([-21.607084, -194.545169]).addTo(mymap);
+        marker.bindPopup("<b>Baie des Tortues, Bourail</b><br><a href=''>Cliquez pour plus d'infos</a>");
+
+        var popup = L.popup()
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent("Vous avez cliqué à " + e.latlng.toString())
+                .openOn(mymap);
+        }
+
+        mymap.on('click', onMapClick );
+
+    </script>
 <?php $content = ob_get_clean(); ?>
 <?php include 'layout.php'; ?>
--- view/layout.php
+++ view/layout.php
@@ -8,12 +8,19 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="shortcut icon" type="image/x-icon" href="../graphs/meteoncie.ico" />
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
-<link rel="stylesheet" href="../index.css"/>
+<?php if($login == "Aphaz")
+{
+    echo '<link rel="stylesheet" href="../index2.css"/>';
+}
+else
+{
+    echo '<link rel="stylesheet" href="../index.css"/>';
+} ?>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 </head>
 
 <body>
-<header>
+<header class="container">
     <a href="../index.php"><img id='titre' src="../graphs/titre.png"></a>
 
     <?php
@@ -33,7 +40,7 @@
         }
     }?>
 
-</>
+</header>
 
 <?php echo $content; ?>
 
