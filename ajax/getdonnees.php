aa7d57cf04513ffba680ca658260b322b7254315
TheBody983 <61261544+TheBody983@users.noreply.github.com>
2020-10-01 11:51:17 +1100

    hotfix ajax getdonnees

--- ajax/getdonnees.php
+++ ajax/getdonnees.php
@@ -1,13 +1,9 @@
 <?php
+require_once '../model.php';
+
 $q = $_GET['q'];
 
-$con = mysqli_connect('localhost','model','1234','meteo_n_cie');
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
-}
-
-$sql="SELECT * FROM mesures WHERE mesure_name = '".$q."'";
-$result = mysqli_query($con,$sql);
+$mesures = get_mesures($q);
 ?>
 <table>
 <tr>
@@ -16,13 +12,12 @@ $result = mysqli_query($con,$sql);
   <th>Valeur</th>
 </tr>
 <?php
-while($row = mysqli_fetch_array($result)) {
+foreach($mesures as $mesure) {
     echo "<tr>";
-    echo "<td>" . $row['date'] . "</td>";
-    echo "<td>" . $row['stationID'] . "</td>";
-    echo "<td>" . $row['mesure_value'] . "</td>";
+    echo "<td>" . $mesure['date'] . "</td>";
+    echo "<td>" . $mesure['stationID'] . "</td>";
+    echo "<td>" . $mesure['value'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
-mysqli_close($con);
 ?>
